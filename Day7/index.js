422c01229a279ae9cf990c91990b600bc7b68f30
shivaranjanijayanna <shivaranjanijayanna@gmail.com>
2024-12-27 01:01:12 -0500

    day7 of nodejs

---
+++ Day7/index.js
@@ -0,0 +1,22 @@
+import http from "http";
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    const url = req.url;
+
+    if (url === "/about") {
+      res.write("Welcome to about us page");
+      res.end();
+    } else if (url === "/contact") {
+      res.write("Welcome to contact us page");
+      res.end();
+    } else {
+      res.write("Hello World!");
+      res.end();
+    }
+  })
+  .listen(3010, function () {
+    console.log("server start at port 3010");
+  });
